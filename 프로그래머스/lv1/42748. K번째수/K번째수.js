c78ca2cab4997c0fe52432c09bd8453a2763da21
elmo-can <120288440+elmo-can@users.noreply.github.com>
2023-01-13 18:13:01 +0900

    Update K번째수.js

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,4 +1,3 @@
 function solution(array, commands) {
-   return commands.map((el)=>(array.slice(el[0]-1,el[1]).sort((x,y)=>x-y)[el[2]-1]));
-    
+   return commands.map((el)=>(array.slice(el[0]-1,el[1]).sort((x,y)=>x-y)[el[2]-1]));    
 }
