8194cd059e786db6fc0546209105d5032e0f7424
elmo-can <120288440+elmo-can@users.noreply.github.com>
2023-01-13 13:51:59 +0900

    Update 소인수분해.js

--- 백준/Bronze/11653. 소인수분해/소인수분해.js
+++ 백준/Bronze/11653. 소인수분해/소인수분해.js
@@ -2,11 +2,10 @@ var fs = require('fs');
 var input = fs.readFileSync('/dev/stdin').toString().split(' ');
 
 let n = parseInt(input[0]);
+let i = 2;
 
 function recursive(n) {
   if (n == 1) return;
-
-  let i = 2;
   while (n >= i) {
     if (n % i == 0) {
       console.log(i);
