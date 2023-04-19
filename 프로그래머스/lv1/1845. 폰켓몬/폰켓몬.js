6a7ca26abc812b259e1f1ecb2e0bc35fabb898f5
Chan Lee <120288440+1eecan@users.noreply.github.com>
2023-04-19 12:01:20 +0900

    [level 1] Title: 폰켓몬, Time: 0.22 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,5 +1,5 @@
 function solution(nums) {
     let length = nums.length/2;
-    nums = [...new Set(nums)];
-    return nums.length<length?nums.length:length;
+    nums = new Set(nums);
+    return nums.size<length?nums.size:length;
 }
