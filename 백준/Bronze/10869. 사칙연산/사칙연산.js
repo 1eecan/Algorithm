86cdda6f603eb12367dde6016dfe4a5511d4fb8d
Chan Lee <120288440+1eecan@users.noreply.github.com>
2023-04-26 13:30:40 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,8 @@
+let input = require("fs").readFileSync('/dev/stdin').toString().split("\n");
+let a = parseInt(input[0].split(' ')[0]);
+let b = parseInt(input[0].split(' ')[1]);
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(parseInt(a/b));
+console.log(a%b);
