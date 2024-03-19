84f1de50cb04bae8a016054878aa82957cdcb005
Chan Lee <120288440+1eecan@users.noreply.github.com>
2024-03-19 21:08:27 +0900

    [level 3] Title: 네트워크, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,18 +1,18 @@
 function solution(n, computers) {
-    let visited = Array(n).fill(false);
+    const visited = new Array(n).fill(false);
     let count = 0;
     
-    function dfs(current) {
+    function dfs(current){
         visited[current] = true;
-        for (let i = 0; i < n; i++) {
-            if (computers[current][i] === 1 && !visited[i]) {
+        for(let i=0;i<n;i++){
+            if(computers[current][i] === 1 && !visited[i]){
                 dfs(i);
             }
         }
     }
     
-    for (let i = 0; i < n; i++) {
-        if (!visited[i]) {
+    for(let i=0;i<n;i++){
+        if(!visited[i]){
             dfs(i);
             count++;
         }
