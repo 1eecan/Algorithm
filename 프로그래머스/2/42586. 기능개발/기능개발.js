4c5908abed7b5fb11fcf9788da2d8f8ba26b367c
Chan Lee <120288440+1eecan@users.noreply.github.com>
2024-03-25 17:42:19 +0900

    [level 2] Title: 기능개발, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,16 @@
+function solution(progresses, speeds) {
+    var answer = [];
+    while(progresses.length){
+        let count = 0;
+        for(let i=0;i<progresses.length;i++){
+            progresses[i]+=speeds[i];
+        }
+        while(progresses[0]>=100){
+            progresses.shift();
+            speeds.shift();
+            count++;
+        }
+        if(count!==0) answer.push(count);
+    }
+    return answer;
+}
