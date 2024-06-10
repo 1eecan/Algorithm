2f9b67edfef381564b0f0a1b107c8be17b3a6034
Chan Lee <120288440+1eecan@users.noreply.github.com>
2024-06-10 16:46:21 +0900

    [level 0] Title: 배열 조각하기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
@@ -1,4 +1,4 @@
 function solution(arr, query) {
-    query.forEach((el,index)=>index%2===0 ? arr.splice(el+1,arr.length+1) : arr.splice(0,el));
+    query.forEach((el,index)=>index%2===0 ? arr.splice(el+1) : arr.splice(0,el));
     return arr;
 }
